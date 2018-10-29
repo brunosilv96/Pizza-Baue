a2f68ee735e948edc7f39726b1b603c70c36a730
Gabrielle Torres <gabitorres118@gmail.com>
2018-10-29 20:28:29 -0300

    Incluindo a página Index.html com CSS e JS

---
+++ Site/js/global.js
@@ -0,0 +1,69 @@
+   window.onscroll = function() {
+    menuSticky()
+};
+function menuSticky() {
+    var navbar = document.querySelector("nav");
+    if (window.pageYOffset >= 0.7) {
+        navbar.classList.add("scrolling");
+    } else {
+        navbar.classList.remove("scrolling");
+    }
+}
+
+
+
+var responsiveSlider = function() {
+var slider = document.getElementById("slider");
+var sliderWidth = slider.offsetWidth;
+var slideList = document.getElementById("slideWrap");
+var count = 1;
+var items = slideList.querySelectorAll("#itemsSlider").length-1;
+var prev = document.getElementById("prev");
+var next = document.getElementById("next");
+
+window.addEventListener('resize', function() {
+  sliderWidth = slider.offsetWidth;
+});
+
+var prevSlide = function() {
+  if(count > 1) {
+    count = count - 2;
+    slideList.style.left = "-" + count * sliderWidth + "px";
+    count++;
+  }
+  else if(count = 1) {
+    count = items - 1;
+    slideList.style.left = "-" + count * sliderWidth + "px";
+    count++;
+  }
+};
+
+var nextSlide = function() {
+  if(count < items) {
+    slideList.style.left = "-" + count * sliderWidth + "px";
+    count++;
+  }
+  else {
+    count = items;
+    slideList.style.left = "0px";
+    count = 1;
+  }
+};
+
+next.addEventListener("click", function() {
+  nextSlide();
+});
+
+prev.addEventListener("click", function() {
+  prevSlide();
+});
+
+setInterval(function() {
+  nextSlide()
+}, 8000);
+
+};
+
+window.onload = function() {
+responsiveSlider();  
+}
