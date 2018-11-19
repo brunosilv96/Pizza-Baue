cb25ca54303255f81a1427b86abdbacc5c43217f
Bruno Silva <brunocosta231@gmail.com>
2018-11-19 12:10:52 -0200

    Algumas coisas de PHP

--- Site/php/cadastro.php
+++ Site/php/cadastro.php
@@ -1,8 +1,12 @@
 <?php
 /* Faz a requisição da Biblioteca do Banco de Dados */
 require_once 'lib/bancoDeDados.php';
+require_once 'class/usuario.php';
 
+$user1 = new Usuario();
 
+$user1->setNome("Bruno");
 
+echo ($user1->getNome());
 
 ?>
