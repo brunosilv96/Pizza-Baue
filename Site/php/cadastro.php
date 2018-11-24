a1ef339ede8fb647eb06bfcf6c0a5238e2f39d63
Cintia Martins <cintia.martins7@hotmail.com>
2018-11-23 22:37:32 -0200

    Cadastro de usuario PHP

--- Site/php/cadastro.php
+++ Site/php/cadastro.php
@@ -3,32 +3,45 @@
 require_once 'class/Usuario.php';
 require_once 'lib/bancoDeDados.php';
 
-
-
 /* Faz a 'validação' dos campos em $_POST */
 $campos = Array("txtEmail", "txtCpf", "senha1", "senha2");
 
 if(verificaForm($campos) == true){
-    
+    /* Cria um objeto do tipo Usuario */
     $user = new Usuario();
     
+    /* Seta os valores para a classe */
     $user->setNome($_POST["txtNome"]);
-    $user->setNome($_POST["txtEmail"]);
-    $user->setNome($_POST["txtCpf"]);
-    $user->setNome($_POST["senha1"]);
+    $user->setEmail($_POST["txtEmail"]);
+    $user->setCpf($_POST["txtCpf"]);
+    $user->setSenha($_POST["senha1"]);
     
+    /* Inicia o processo de adição dos dados no Banco */
     try {
+        /* Cria um objeto do tipo Banco de Dados */
         $conex = new BancoDeDados();
         
-        $conex->abrirConexao();
-        
+                
+        if ($conex->abrirConexao()){ 
+           /*Atribuindo os valores da classe a variaveis*/ 
+           $nome = $user->getNome();
+           $email = $user->getEmail();
+           $cpf = $user->getCpf();
+           $senha = $user->getSenha();
+           
+           /*inserindo os dados no banco*/
+           $conex-> executarSql("INSERT usuario(nome, email, cpf, senha) VALUES ('$nome', '$email', '$cpf', MD5('$senha'));");
+           
+
+           header("Location: exibeMsg.php?txtresultado=Sucesso");
+
+        }else{
+            echo "Erro de conexão com o Banco de Dados";
+        }
         
     } catch (Exception $e) {
-        
+        echo "Erro ao salvar os dados no Banco " . $e;
     }
-    
-    
-
     
 }else{
     echo "Erro nos campos do formulário";
---
+++ Site/php/exibeMsg.php
@@ -0,0 +1,18 @@
+<html>
+	<head>
+		<meta charset="UTF-8">
+		<title>Mensagem</title>
+	</head>
+	<body>
+		<div>
+			<div class="mensagem">
+				<h1>MENSAGEM</h1>
+			</div>
+			<div class="contMensagem">
+				<?php
+				 echo $_GET['txtresultado'];
+                ?>
+			</div>
+		</div>
+	</body>
+</html>
